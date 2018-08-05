a62f99c74aa211f4861027aafa8192e811a81380
akbar <hc.akbar@gmail.com>
2018-08-05 16:36:34 -0400

    cleaning up code

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
+/*
+ * Create a list that holds all of your cards
+ */
 let cardIcons = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"];
+
 let openCards = [];
 let matchedCards = [];
-//TODO move this to inside the methods
+let totalMoves;
+let totalTime;
 
 document.addEventListener('DOMContentLoaded', function () {
 
@@ -12,14 +17,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-/*
- * Create a list that holds all of your cards
- */
-
 
 function displayCards(arrays) {
     const cardContainer = document.querySelector('.deck');
-    // let openCards = [];
 
     for (let i = 0; i < arrays.length; i++) {
         const card = document.createElement('li');
@@ -27,53 +27,67 @@ function displayCards(arrays) {
         card.innerHTML = "<i class='" + arrays[i] + "'></i>";
         cardContainer.appendChild(card);
 
-        card.addEventListener('click', function () {
-            const currentOpenCard = this;
-            const previousOpenCard = openCards[0];
+        //click event to each card
+        click(card);
+    }
+}
 
-            if (openCards.length === 1) {
-                //show open card
-                card.classList.add('open', 'show');
+//click on card event
+function click(card) {
+    card.addEventListener('click', function () {
+        const currentOpenCard = this;
+        const previousOpenCard = openCards[0];
 
-                //add open card in array
-                openCards.push(this);
+        if (openCards.length === 1) {
+            //show open card
+            card.classList.add('open', 'show');
 
-                //compare two open cards
-                if (currentOpenCard.innerHTML === previousOpenCard.innerHTML) {
-                    //show if matched
-                    console.log("matched"); //TODO
-                    currentOpenCard.classList.add('match');
-                    previousOpenCard.classList.add('match');
+            //add open card in array
+            openCards.push(this);
 
-                    matchedCards.push(currentOpenCard, previousOpenCard);
+            //compare card
+            compareCard(currentOpenCard, previousOpenCard);
 
-                    openCards = [];
+        } else {
+            //show open card
+            currentOpenCard.classList.add('open', 'show');
 
-                    isGameOver();
-                } else {
-                    //don't show if not matched
-                    openCards = [];
-                    console.log("not matched"); //TODO
-                    setTimeout(function () {
-                        currentOpenCard.classList.remove('open', 'show');
-                        previousOpenCard.classList.remove('open', 'show');
-                    }, 500);
-                }
-            } else {
-                //show open card
-                currentOpenCard.classList.add('open', 'show');
+            //add open card in array
+            openCards.push(this);
+        }
+    })
+}
 
-                //add open card in array
-                openCards.push(this);
-            }
-        })
+function compareCard(currentOpenCard, previousOpenCard) {
+    //compare two open cards
+    if (currentOpenCard.innerHTML === previousOpenCard.innerHTML) {
+        //show if matched
+        console.log("matched"); //TODO
+        currentOpenCard.classList.add('match');
+        previousOpenCard.classList.add('match');
+
+        matchedCards.push(currentOpenCard, previousOpenCard);
+
+        openCards = [];
+
+        isGameOver();
+    } else {
+        //don't show if not matched
+        console.log("not matched"); //TODO
+        setTimeout(function () {
+            currentOpenCard.classList.remove('open', 'show');
+            previousOpenCard.classList.remove('open', 'show');
+            openCards = [];
+        }, 300);
     }
 }
 
 function isGameOver() {
-    if (cardIcons.length === matchedCards.length) {
-        alert('game over'); //TODO
-    }
+    setTimeout(function () {
+        if (cardIcons.length === matchedCards.length) {
+            alert('Game Over\nHooray! You mated all cards.\nTotal Moves: ' + totalMoves + '.\nTotal Time: ' + totalTime); //TODO
+        }
+    }, 300)
 }
 
 /*
@@ -97,7 +111,6 @@ function shuffle(array) {
 
     return array;
 }
-
 
 /*
  * set up the event listener for a card. If a card is clicked:
